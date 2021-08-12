f2fff97995063da81b93971f044f39ba173b8089
ldh7228 <ldh7228@gmail.com>
2021-08-12 15:32:46 +0900

    Add circle

--- src/index.js
+++ src/index.js
@@ -4,3 +4,11 @@ const ctx = canvas.getContext('2d');
 canvas.width = 1500;
 canvas.height = 700;
 
+let circleX = 750;
+let circleY = 350;
+
+setInterval(function () {
+    ctx.arc(circleX, circleY, 20, 0, Math.PI*2);
+    ctx.fillStyle = "blue";
+    ctx.fill();
+}, 20)

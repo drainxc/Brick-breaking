d5e146e126cce69596f982dca65c02547a03154b
ldh7228 <ldh7228@gmail.com>
2021-08-13 13:54:22 +0900

    Add comments

--- src/index.js
+++ src/index.js
@@ -20,14 +20,14 @@ function circle() {
     ctx.arc(circleX, circleY, radius, 0, Math.PI * 2);
     ctx.fillStyle = "blue";
     ctx.fill();
-    ctx.closePath();
+    ctx.closePath(); // 공 그리기
     if (game && !death) {
         if (circleX - radius < 0 || circleX + radius > canvas.width) {
             speedX *= -1;
         }
         if (circleY - radius < 0 || (circleY + radius > playerY && circleX + radius > playerX && circleX - radius < playerX + 250)) {
             speedY *= -1;
-        }
+        } // 벽이나 바에 튕겼을 때
         if (circleY + radius > canvas.height) {
             if (!death) {
                 let input = confirm('gameover\n다시 하시겠습니까?');
@@ -36,12 +36,12 @@ function circle() {
                 }
                 death = true;
                 circleY = canvas.height - radius;
-            }
+            } // 죽었을 때
         }
         if (!death) {
             circleY += speedY;
             circleX += speedX; 
-        }
+        } // 공 속도
     }
 }
 
@@ -50,23 +50,23 @@ function bar() {
     if (playerX + 250 >= canvas.width) { playerX = canvas.width - 250; }
     ctx.fillStyle = 'blue';
     ctx.fillRect(playerX, playerY, 250, 20);
-}
+} // 바 그리기
 
 function onMove(event) {
     playerX = event.offsetX - 125;
-}
+} // 마우스의 X좌표
 
 function start() {
     if (!game && !death) {
         game = true;
     }
-}
+} // 게임 시작
 
 setInterval(function () {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     circle();
     bar();
-}, 20)
+}, 20) // 그리기
 
 document.addEventListener('click', start);
 document.addEventListener('mousemove', onMove);
